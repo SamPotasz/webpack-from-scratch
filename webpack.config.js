8a50f15363d2ebfad315ce434883100fcbbf666d
SamPotasz <sampotasznik@gmail.com>
2020-05-12 11:27:12 -0500

    Got webpack working

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], //order matters here!
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        }
+      }
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+}
